2fd912742013051adb1af53308ce26661ae7b6a9
Sadanandam-dell <msadha431@gmail.com>
2025-10-28 15:06:02 +0530

    day-2

--- day-2/main.tf
+++ day-2/main.tf
@@ -1,20 +1,20 @@
 resource "aws_vpc" "name" {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = var.aws_vpc
     tags = {
-      Name ="one"
+      Name ="one1"
     }
 }
 resource "aws_subnet" "name" {
     vpc_id = aws_vpc.name.id
-    cidr_block = "10.0.0.0/24"
+    cidr_block = var.aws_subnet
     tags = {
-        Name = "my subnet"
+        Name = "my subnet1"
       
     }
   
 }
 resource "aws_instance" "name" {
-    ami ="ami-07860a2d7eb515d9a"
+    ami = var.ami_id
     instance_type = "t3.micro"
     tags = {
         Name = "ec2-1"
