6c225f2ce137aeec71d370e3647871fb64fb475b
Sadanandam-dell <msadha431@gmail.com>
2025-10-30 12:49:01 +0530

    day-4

---
+++ day-4-state-locking/main.tf
@@ -0,0 +1,26 @@
+
+resource "aws_instance" "name" {
+    ami = var.ami_id
+    instance_type = "t3.micro"
+    tags = {
+        Name = "ec2-12"
+      
+    }
+  
+}
+
+resource "aws_vpc" "name" {
+    cidr_block = var.aws_vpc
+    tags = {
+      Name ="one12"
+    }
+}
+resource "aws_subnet" "name" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = var.aws_subnet
+    tags = {
+        Name = "my subnet12"
+      
+    }
+  
+}
---
+++ day-4-state-locking/terraform.tfvars
@@ -0,0 +1,4 @@
+ami_id = "ami-0bdd88bd06d16ba03"
+type = "t3.micro"
+aws_vpc = "10.0.0.0/16"
+aws_subnet = "10.0.0.0/24"
