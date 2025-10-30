9338cb69065889aaa460ded7c9c750319e91e5aa
Sadanandam-dell <msadha431@gmail.com>
2025-10-30 12:37:48 +0530

    day-3

---
+++ day-3-statefile/main.tf
@@ -0,0 +1,26 @@
+
+resource "aws_instance" "name" {
+    ami = var.ami_id
+    instance_type = "t3.micro"
+    tags = {
+        Name = "ec2-11"
+      
+    }
+  
+}
+
+resource "aws_vpc" "name" {
+    cidr_block = var.aws_vpc
+    tags = {
+      Name ="one11"
+    }
+}
+resource "aws_subnet" "name" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = var.aws_subnet
+    tags = {
+        Name = "my subnet11"
+      
+    }
+  
+}
---
+++ day-3-statefile/variable.tf
@@ -0,0 +1,26 @@
+variable "ami_id" {
+    description = "passing ami values"
+    default = ""
+    type = string
+  
+}
+
+
+variable "type" {
+  description = "type of instance"
+  default = ""
+  type = string
+}
+
+variable "aws_vpc" {
+    description = "passing vpc values"
+    default = ""
+    type = string  
+}
+
+variable "aws_subnet" {
+    description = "passing subnet values"
+    default = ""
+    type = string  
+  
+}
