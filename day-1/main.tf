206855b7e13e3569d4be76782b6fa25f546d7d08
sadanand-cloud-tech <msadha431@gmail.com>
2025-11-01 19:41:48 +0530

    Add VPC and subnet resources in main.tf

--- day-1/main.tf
+++ day-1/main.tf
@@ -1,9 +1,12 @@
+#creating vpc
 resource "aws_vpc" "name" {
     cidr_block = "10.0.0.0/16"
     tags = {
       Name ="one"
     }
 }
+
+#creating subnet
 resource "aws_subnet" "name" {
     vpc_id = aws_vpc.name.id
     cidr_block = "10.0.0.0/24"
