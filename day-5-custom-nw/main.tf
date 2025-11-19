82bf2068e3e5529b7d8a822d7976a395ba9915b7
sadanand-cloud-tech <msadha431@gmail.com>
2025-11-20 00:06:38 +0530

    Change availability zones for subnets to us-west

--- day-5-custom-nw/main.tf
+++ day-5-custom-nw/main.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "cust_vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.cust_vpc.id
   cidr_block              = "10.0.0.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "us-west-1a"
   map_public_ip_on_launch = true
   tags = {
     Name = "public-subnet"
@@ -21,7 +21,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.cust_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1b"
+  availability_zone = "us-west-1b"
   tags = {
     Name = "private-subnet"
   }
